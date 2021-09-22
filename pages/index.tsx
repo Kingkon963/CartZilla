2fd875f0a79f2ed092d722a642df2146dd732c55
Naeem Ashraf <naeem.ashraf@akqa.com>
2021-09-23 09:59:08 +1000

    Move limited offers to separate component

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import Layout from "../components/Layout";
 import Price from "../components/Price";
 import CompanySlides from "../components/CompanySlides";
 import ProductCard from "../components/ProductCard";
+import LimitedOffer from "../components/LimitedOffer";
 import keyGen from "../utils/genKey";
 
 import HomeHeadCarousel from "../components/HomeHeadCarousel";
@@ -334,43 +335,7 @@ const Home: NextPage = () => {
 
         {/* Limited Offer */}
         <div className="xl:h-vh-50 px-3 xl:px-container xxl:px-containerXXL mt-7">
-          <div
-            className="w-full h-full flex flex-col xl:flex-row rounded"
-            style={{ backgroundColor: "#EEF8FE" }}
-          >
-            <div className="w-full xl:w-2/6">
-              <div className="flex flex-col h-full justify-center items-start pl-6 xl:pl-10">
-                <div className="bg-primary text-white px-2 text-sm rounded my-7 xl:my-0 xl:mb-10">
-                  Limited Offer
-                </div>
-                <h1 className="font-thin text-2xl xl:text-3xl">All new</h1>
-                <h1 className="font-bold text-3xl xl:text-4xl">
-                  Last Gen iPad Pro
-                </h1>
-                <h3 className="font-thin text-lg xl:text-xl tracking-wider">
-                  at discounted price. Hurry up!
-                </h3>
-                <span
-                  className="tracking-widest py-5 text-sm text-gray-500"
-                  style={{ letterSpacing: "1rem" }}
-                >
-                  dhms
-                </span>
-                <button className="bg-blue-600 text-white px-6 py-2 rounded">
-                  View Offers
-                </button>
-              </div>
-            </div>
-            <div className="w-full xl:w-4/6 flex items-center justify-end">
-              <Image
-                src="https://cartzilla.createx.studio/img/home/banners/offer.jpg"
-                width="700"
-                height="401"
-                alt="offer"
-                layout="intrinsic"
-              />
-            </div>
-          </div>
+          <LimitedOffer />
         </div>
 
         {/* Company Slides */}
