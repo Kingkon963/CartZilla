06b9597f59934fd0174e5c5e976ed82c38393810
Muhammad Naimul Islam <naimulislam19149@gmail.com>
2021-10-03 18:45:26 +0600

    Stepper setup done

---
+++ pages/checkout.tsx
@@ -0,0 +1,50 @@
+import * as React from "react";
+import { FC, useState } from "react";
+import type { NextPage } from "next";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+import Layout from "../components/Layout";
+import Breadcrumb from "../components/Breadcrumb";
+import CartList from "../components/Cart/CartList";
+import CartStepper from "../components/Cart/CartStepper";
+import StepperLabels from "../components/Cart/CartStepperLabels";
+import CartDetails from "../components/Cart/CartDetails";
+import CartShipping from "../components/Cart/CartShipping";
+
+const Checkout: NextPage = () => {
+  const [activeStep, setActiveStep] = useState(1);
+
+  const CartContents = [CartDetails, CartShipping];
+
+  const Content = CartContents[activeStep - 1];
+  return (
+    <Layout>
+      <div className="bg-secondary text-white px-3 xl:px-container xxl:px-containerXXL py-5 lg:pt-10 flex flex-col gap-5">
+        <div className="flex flex-col lg:flex-row justify-between items-center">
+          <h1 className="text-2xl lg:text-3xl order-2 lg:order-1 mt-3 lg:mt-0 font-medium">
+            Checkout
+          </h1>
+          <Breadcrumb
+            dark={true}
+            links={[{ title: "Shop", url: "/shopcategories" }]}
+            currentPage="Cart"
+          />
+        </div>
+        <div className="lg:w-8/12">
+          <CartStepper
+            labels={StepperLabels}
+            activeStep={activeStep}
+            setActiveStep={setActiveStep}
+          />
+        </div>
+      </div>
+
+      <div className="px-3 xl:px-container xxl:px-containerXXL mb-16">
+        <Content />
+      </div>
+    </Layout>
+  );
+};
+
+export default Checkout;
